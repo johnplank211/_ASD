e656e92c9e66423bf8c7da34fda19ed23433e13c
John plank <johnplank211@gmail.com>
2012-09-26 16:31:35 -0400

    Added more stuff
    somethings working

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 
-
+console.log("bit me");
 
 $('#home').on('pageinit', function(){
 	//code needed for home page goes here
+
+console.log("eat it");
+
 });	
 		
 $('#form').on('pageinit', function(){
@@ -23,7 +26,7 @@ $('#form').on('pageinit', function(){
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
-var autofillData = function (){
+var autoFillData = function (){
 	 for ( var n in json) {
 			var id = Math.floor(Math.random()* 1000001);
 			localStorage.setItem(id, JSON.stringify(json[n]));
@@ -31,7 +34,7 @@ var autofillData = function (){
 };
 
 var getData = function(){
-	console.log("F U");
+	console.log("yes");
 		//changePage("savedData");
 		//toggleControls("on");
 		if (localStorage.length === 0) {
@@ -64,13 +67,51 @@ var getData = function(){
 			//makeItemLinks(localStorage.key(i),  linksLi);
 			//console.log(i);
 		}
-		changePage("savedData");
+		//changePage("savedData");
 		//refreshList();
-		//console.log(obj);	
+		console.log("tard");	
 };
 
+/*var changePage = function(pageId){
+		console.log(pageId);
+		$('#'+ pageId).trigger('pageinit');
+		$.mobile.changePage($('#' + pageId), {transition:"slide"});
+};*/
+
+
 var storeData = function(data){
-	
+	if(!key) {
+			var id    		= Math.floor(Math.random()* 1000001);
+		} else {
+			var id = key;
+		}
+		getCheckBoxValue();
+		getRadio();
+		var item 			= {};
+		    item.apocalypse = ["Apocalypse:", apocalypseValue];
+			item.fear       = ["Fear level:", $("#groups").value];
+			item.firearm	= ["Firearm:", firearmValue];
+			item.ammo		= ["Ammo:", ammoValue];
+			item.melee 		= ["Melee weapon:", meleeWeaponValue];
+			item.canned		= ["Canned:", cannedValue];
+			item.water		= ["Water:", waterValue];
+			item.chain		= ["Chain mesh suit:", chainMeshSuitValue];
+			item.map 		= ["Topographical Map:", topographicalMapValue];
+			item.leatherman = ["Leatherman:", leathermanValue];
+			item.rucksack	= ["Rucksack:", rucksackValue];
+			item.boots		= ["Boots:", bootsValue];
+			item.matches	= ["Matches:", matchesValue];
+			item.p38		= ["P38:", p38Value];
+			item.intestinal = ["Intestinal Fortitude:", intestinalFortitudeValue];
+			//item.item		= ["Item:", ge("item").value];			
+			item.date       = ["World Ended:", $("#date").value];
+			item.email		= ["Email:", $("#email").value];
+			item.comments	= ["Comments:", $("#comments").value];
+			item.readiness  = ["Readiness:", $("#readiness").value];
+
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Checklist Saved!");
+	console.log(item);
 }; 
 
 var	deleteItem = function (){
@@ -78,8 +119,16 @@ var	deleteItem = function (){
 };
 					
 var clearLocal = function(){
-
+	if (localStorage.length === 0) {
+				alert("All clear.")
+				}else{
+					localStorage.clear();
+					window.location.reload();
+					return false;
+			}
 };
+
+var validate;
 
 var displayLink = $("#displayLink");
 		displayLink.on("click", getData);
@@ -88,5 +137,5 @@ var displayLink = $("#displayLink");
 		var save = $("#submit");
 		save.on("click", validate);
 
-
+/*});*/
 
